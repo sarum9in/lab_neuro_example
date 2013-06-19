6df36bc225d52f508f5ec95bb3ca31e6fb5487b8
Aleksey Filippov <sarum9in@gmail.com>
2013-06-19 22:14:44 +0400

    MainWindow: Recognition tab implementation.

--- Gui/MainWindow.cpp
+++ Gui/MainWindow.cpp
@@ -1,6 +1,8 @@
 #include "MainWindow.hpp"
 #include "ui_MainWindow.h"
 
+#include "RecognitionController.hpp"
+
 #include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent):
@@ -8,6 +10,17 @@ MainWindow::MainWindow(QWidget *parent):
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    // Recognition
+    ui->gridField->setSize(ui->gridSize->value());
+    ui->symbolSet->setValidator(new QRegExpValidator(QRegExp("\\w*"), ui->symbolSet));
+    RecognitionController *recognitionController = new RecognitionController(this);
+    connect(ui->symbolSet, &QLineEdit::textChanged, recognitionController, &RecognitionController::setSymbolSet);
+    connect(ui->fixRecognitionButton, &QPushButton::clicked, recognitionController, &RecognitionController::fix);
+    ui->recognizedValue->setModel(recognitionController->model());
+    connect(ui->recognizedValue, &QComboBox::currentTextChanged, recognitionController, &RecognitionController::suggestSymbol);
+    connect(recognitionController, &RecognitionController::recognized, ui->recognizedValue, &QComboBox::setCurrentText);
+    connect(ui->gridSize, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged), recognitionController, &RecognitionController::setSize);
+    connect(ui->gridField, &GridField::dataChanged, recognitionController, &RecognitionController::setData);
 }
 
 MainWindow::~MainWindow()
